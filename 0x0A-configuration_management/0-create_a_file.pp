1cd0e5c1e796ff6f763b75599da0e9f09c6a011e
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2023-09-22 17:00:32 +0100

    task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 file { '/tmp/school':
-  ensure  => file,
+  ensure  => 'file',
+  content => 'I love Puppet',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet',
 }
