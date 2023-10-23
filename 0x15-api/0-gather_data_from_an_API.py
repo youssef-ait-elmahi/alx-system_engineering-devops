27ee3decf2eb6452d5ce2a3715ea9ad4a9e61a20
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2023-10-23 17:50:18 +0100

    task 0 edit 3

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-import requests
-import sys
 """Write a Python script that, using this REST API, for a given employee ID,
 returns information about his/her TODO list progress."""
+import requests
+import sys
 
 
 def get_todo_list_progress(employee_id):
