2bae30f757375f3ff03a91ae8ab015850e973e4d
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2023-10-23 18:36:55 +0100

    tasks 1 and 2

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """
 This script uses the requests module to fetch data from a REST API and exports
-an employee's TODO list progress to a CSV file.
+an employee's TODO list progress to a JSON file.
 """
 
-import csv
+import json
 import requests
 import sys
 
@@ -19,12 +19,13 @@ if __name__ == "__main__":
     todos = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'
                          .format(user_id)).json()
 
-    with open('{}.csv'.format(user_id), 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        for task in todos:
-            writer.writerow([
-                user_id,
-                user.get('name'),
-                task.get('completed'),
-                task.get('title')
-            ])
+    tasks = []
+    for task in todos:
+        tasks.append({
+            "task": task.get('title'),
+            "completed": task.get('completed'),
+            "username": user.get('username')
+        })
+
+    with open('{}.json'.format(user_id), 'w') as jsonfile:
+        json.dump({user_id: tasks}, jsonfile)
