6022e4caf36f704102359258fccfb722a88b36c5
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2023-11-07 15:51:37 +0100

    Add function to get number of subscribers from Reddit API

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Function that queries the Reddit API and returns the number of subscribers
+(not active users, total subscribers) for a given subreddit. If an invalid
+subreddit is given, the function should return 0."""
+import requests
+
+def number_of_subscribers(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'custom'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        return 0
+    return response.json()['data']['subscribers']
